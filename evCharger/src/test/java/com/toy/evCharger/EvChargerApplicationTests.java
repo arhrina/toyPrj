2c306ebae8c075f5de29252069325ee52c37dfca
MW <hyoukim@naver.com>
2020-12-16 15:39:17 +0900

    공공api 활용 토이플젝
    전기차 충전소 확인용 플젝

---
+++ evCharger/src/test/java/com/toy/evCharger/EvChargerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.evCharger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
