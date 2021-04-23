8bf4d5545591d4d7619c73e34424832184a893c7
MW <hyoukim@naver.com>
2021-04-23 11:35:43 +0900

    queryDSL 예제 테스트

--- jpa/src/main/java/com/test/entity/Member.java
+++ jpa/src/main/java/com/test/entity/Member.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Table(name = "Tb_Member")
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private Long age;
